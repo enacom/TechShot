4dfc24d2b7ab158a47b5175f7ae72b8293a0396c
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2022-03-21 09:21:33 -0300

    feat (des_model.py): create clear method and implement terminal/port queue model (FIFO)

--- des_model.py
+++ des_model.py
@@ -21,6 +21,17 @@ class DES_model:
         self.train_count = nt
         self.train_speed = v
         self.train_load = L
+        self.clear()
+
+    def clear(self):
+        ''' Clear model to start a new simulation.
+        '''
+
+        # queue
+        nt = self.distance.shape[1]
+        np = self.distance.shape[0]
+        self.terminal_queue = [[0] for _ in range(nt)]
+        self.port_queue = [[0] for _ in range(np)]
 
     def starting_events(self, simulator):
         ''' Add starting events to simulator calendar.
@@ -30,13 +41,15 @@ class DES_model:
         '''
 
         # add starting events
-        for i in range(len(self.train_count)):
-            for im in range(self.train_count[i]):
+        id = 0
+        for im in range(len(self.train_count)):
+            for i in range(self.train_count[im]):
                 ip = 0
                 it = self.dispatch_to_terminal()
                 t = simulator.time + self.distance[ip, it] / self.train_speed[im]
-                data = [ip, it, im]
+                data = [ip, it, im, id]
                 simulator.add_event(t, self.on_finish_unloaded_path, data)
+                id += 1
 
     def on_finish_unloaded_path(self, simulator, data):
         ''' Callback function for finishing unloaded path.
@@ -46,12 +59,15 @@ class DES_model:
             data (list): port, terminal and train model indexes.
         '''
 
-        print('Loading at terminal {}'.format(data[1]))
+        print('{:02.0f}:{:02.0f} Train {} arrived at terminal {}'.format(simulator.time // 3600,
+                                                                         (simulator.time % 3600) // 60, data[3],
+                                                                         data[1]))
 
         # add new event
         it = data[1]  # terminal index
         im = data[2]  # train model index
-        t = simulator.time + self.loading_time[it, im]  # terminal loading time
+        t = max(simulator.time, self.terminal_queue[it][-1]) + self.loading_time[it, im]  # terminal loading time
+        self.terminal_queue[it].append(t)
         simulator.add_event(t, self.on_finish_loading, data)
 
     def on_finish_loading(self, simulator, data):
@@ -62,7 +78,9 @@ class DES_model:
             data (list): port, terminal and train model indexes.
         '''
 
-        print('Going from terminal {} to port {}'.format(data[1], data[0]))
+        print('{:02.0f}:{:02.0f} Train {} going from terminal {} to port {}'.format(simulator.time // 3600,
+                                                                                    (simulator.time % 3600) // 60,
+                                                                                    data[3], data[1], data[0]))
 
         # add new event
         ip = self.dispatch_to_port()  # port index
@@ -80,12 +98,14 @@ class DES_model:
             data (list): port, terminal and train model indexes.
         '''
 
-        print('Unoading at port {}'.format(data[0]))
+        print('{:02.0f}:{:02.0f} Train {} arrived at port {}'.format(simulator.time // 3600,
+                                                                     (simulator.time % 3600) // 60, data[3], data[0]))
 
         # add new event
         ip = data[0]  # port index
         im = data[2]  # train model index
-        t = simulator.time + self.unloading_time[ip, im]  # port unloading time
+        t = max(simulator.time, self.port_queue[ip][-1]) + self.unloading_time[ip, im]  # port unloading time
+        self.port_queue[ip].append(t)
         simulator.add_event(t, self.on_finish_unloading, data)
 
     def on_finish_unloading(self, simulator, data):
@@ -96,7 +116,9 @@ class DES_model:
             data (list): port, terminal and train model indexes.
         '''
 
-        print('Going from port {} to terminal {}'.format(data[0], data[1]))
+        print('{:02.0f}:{:02.0f} Train {} going from port {} to terminal {}'.format(simulator.time // 3600,
+                                                                                    (simulator.time % 3600) // 60,
+                                                                                    data[3], data[0], data[1]))
 
         # add new event
         it = self.dispatch_to_terminal()  # terminal index
